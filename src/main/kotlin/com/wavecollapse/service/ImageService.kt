7e2bf1ccbaaa0d14fae74868960c15ebca38ed90
myakisheff <n.s.myakishev@yandex.ru>
2024-02-07 22:45:02 +0300

    Init of services

---
+++ src/main/kotlin/com/wavecollapse/service/ImageService.kt
@@ -0,0 +1,9 @@
+package main.kotlin.com.wavecollapse.service
+
+import main.kotlin.com.wavecollapse.business.Image
+
+class ImageService {
+//    fun default(): Image {
+//        return Image()
+//    }
+}
