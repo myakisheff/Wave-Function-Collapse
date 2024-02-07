34bc0edf6a7dd15d4f974b646443c3fb57a2d1cc
myakisheff <n.s.myakishev@yandex.ru>
2024-02-08 00:26:43 +0300

    Refactored to UUID

--- src/main/kotlin/com/wavecollapse/persistence/repository/ListImageRepository.kt
+++ src/main/kotlin/com/wavecollapse/persistence/repository/ListImageRepository.kt
@@ -1,16 +1,25 @@
 package main.kotlin.com.wavecollapse.persistence.repository
 
 import main.kotlin.com.wavecollapse.business.Image
+import java.util.*
 
 class ListImageRepository : ImageRepository {
     // fake implementation of local storage
     // For ideal: use persistence.model for saving to DB
-    private val images : MutableList<Image> = mutableListOf()
+    private val images : MutableMap<UUID, Image> = mutableMapOf()
+
     override fun save(image: Image) {
-        images.add(image)
+        images[image.id] = image
     }
 
-    override fun getAll(): List<Image> = images
+    override fun getAll(): Map<UUID, Image> = images
+    override fun findById(id: UUID): Image? = images[id]
 
-    override fun findById(id: Int): Image? = images.find { it.id == id }
+    override fun getUniqueID(): UUID {
+        var id = UUID.randomUUID()
+        while(images.containsKey(id))
+            id = UUID.randomUUID()
+        return id
+    }
+
 }
--- src/main/kotlin/com/wavecollapse/persistence/repository/ListTileRepository.kt
+++ src/main/kotlin/com/wavecollapse/persistence/repository/ListTileRepository.kt
@@ -1,16 +1,24 @@
 package main.kotlin.com.wavecollapse.persistence.repository
 
 import main.kotlin.com.wavecollapse.business.Tile
+import java.util.UUID
 
 class ListTileRepository : TileRepository {
     // fake implementation of local storage
     // For ideal: use persistence.model for saving to DB
-    private val tiles : MutableList<Tile> = mutableListOf()
+    private val tiles : MutableMap<UUID, Tile> = mutableMapOf()
+
     override fun save(tile: Tile) {
-        tiles.add(tile)
+        tiles[tile.id] = tile
     }
 
-    override fun getAll(): List<Tile> = tiles
+    override fun getAll(): Map<UUID, Tile> = tiles
 
-    override fun findById(id: Int): Tile? = tiles.find { it.id == id }
+    override fun findById(id: UUID): Tile? = tiles[id]
+    override fun getUniqueID(): UUID {
+        var id = UUID.randomUUID()
+        while(tiles.containsKey(id))
+            id = UUID.randomUUID()
+        return id
+    }
 }
