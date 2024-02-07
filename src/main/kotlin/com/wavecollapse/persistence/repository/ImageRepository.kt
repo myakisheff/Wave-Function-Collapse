e4c0933020538e15c3e8f2b1060994a1ee03931a
myakisheff <n.s.myakishev@yandex.ru>
2024-02-08 00:25:34 +0300

    Refactored to UUID id's

--- src/main/kotlin/com/wavecollapse/persistence/repository/ImageRepository.kt
+++ src/main/kotlin/com/wavecollapse/persistence/repository/ImageRepository.kt
@@ -1,9 +1,11 @@
 package main.kotlin.com.wavecollapse.persistence.repository
 
 import main.kotlin.com.wavecollapse.business.Image
+import java.util.*
 
 interface ImageRepository {
     fun save(image : Image)
-    fun getAll() : List<Image>
-    fun findById(id : Int) : Image?
+    fun getAll() : Map<UUID, Image>
+    fun findById(id : UUID) : Image?
+    fun getUniqueID(): UUID
 }
--- src/main/kotlin/com/wavecollapse/persistence/repository/TileRepository.kt
+++ src/main/kotlin/com/wavecollapse/persistence/repository/TileRepository.kt
@@ -1,9 +1,11 @@
 package main.kotlin.com.wavecollapse.persistence.repository
 
 import main.kotlin.com.wavecollapse.business.Tile
+import java.util.*
 
 interface TileRepository {
     fun save(tile : Tile)
-    fun getAll() : List<Tile>
-    fun findById(id : Int) : Tile?
+    fun getAll() : Map<UUID, Tile>
+    fun findById(id : UUID) : Tile?
+    fun getUniqueID(): UUID
 }
