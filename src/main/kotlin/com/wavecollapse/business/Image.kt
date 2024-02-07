e61e24922142c5f02fd47352c1978e6b6e82ca13
myakisheff <n.s.myakishev@yandex.ru>
2024-02-08 00:24:29 +0300

    Added connections fo tiles

--- src/main/kotlin/com/wavecollapse/business/Image.kt
+++ src/main/kotlin/com/wavecollapse/business/Image.kt
@@ -1,7 +1,21 @@
 package main.kotlin.com.wavecollapse.business
 
+import java.util.UUID
+
 class Image(
-    val id : Int,
-    val tilesMatrix : Array<Array<Tile>>,
+    val id : UUID,
+    val tiles : MutableList<Tile>,
+    val height : Int,
+    val width : Int,
 ) {
+    private val connections: MutableMap<UUID, MutableMap<Side, MutableList<UUID>>> = mutableMapOf()
+
+    fun IsCorrect() : Boolean = connections.size == tiles.size
+
+    fun addConnection(ownTile: Tile, attachedTile: Tile, side: Side)
+    {
+        if(connections.containsKey(ownTile.id))
+            connections[ownTile.id]?.get(side)?.add(attachedTile.id)
+        else connections[ownTile.id] = mutableMapOf(Pair(side, mutableListOf(attachedTile.id)))
+    }
 }
