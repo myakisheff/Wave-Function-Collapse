7244f852850f6c46393d566ac2caf995284c923c
myakisheff <n.s.myakishev@yandex.ru>
2024-02-08 16:15:01 +0300

    Added get by id method

--- src/main/kotlin/com/wavecollapse/service/ImageService.kt
+++ src/main/kotlin/com/wavecollapse/service/ImageService.kt
@@ -11,6 +11,7 @@ class ImageService(
     fun default(tiles : MutableIterable<Tile>): Image {
         val img = Image(
             UUID.randomUUID(),
+            "Example",
             tiles.toMutableList(),
             5,
             5
@@ -22,6 +23,11 @@ class ImageService(
 
     fun get(id: UUID): Image? =
         repository.findById(id)
+
+    fun getByTag(tag: String): Image {
+        val allImages = repository.getAll()
+        return allImages.first { it.tag == tag }
+    }
 
     fun createNewImage(img: Image): Image =
         repository.save(img)
