4d33ab44678cf4914d208d4d7ecbd9c0e74ee2b7
myakisheff <n.s.myakishev@yandex.ru>
2024-02-08 16:14:27 +0300

    Added image info printing

--- src/main/kotlin/com/wavecollapse/presentation/model/ImageViewModel.kt
+++ src/main/kotlin/com/wavecollapse/presentation/model/ImageViewModel.kt
@@ -6,4 +6,18 @@ class ImageViewModel(
     private val image: Image
 ) {
     fun size() = "Size:\nHeight: ${image.height}\nWidth: ${image.width}"
+    fun tiles() = buildList {
+        image.tiles.forEach { add(it.tile) }
+    }
+
+    fun title() = image.tag
+    fun result() = buildList {
+        image.getImage().forEach { cell ->
+            var line = ""
+            cell.forEach {
+                line += it.tile ?: return listOf("Some tiles empty. Check connections")
+            }
+            add(line)
+        }
+    }
 }
--- src/main/kotlin/com/wavecollapse/presentation/view/ImageSymbolsView.kt
+++ src/main/kotlin/com/wavecollapse/presentation/view/ImageSymbolsView.kt
@@ -4,5 +4,15 @@ import com.wavecollapse.presentation.model.ImageViewModel
 
 fun renderImageView(image: ImageViewModel)
 {
+    println(image.title())
     println(image.size())
+    println()
+    println("Tiles:")
+    image.tiles().forEach{
+        println(it)
+    }
+    println("Result:")
+    image.result().forEach{
+        println(it)
+    }
 }
