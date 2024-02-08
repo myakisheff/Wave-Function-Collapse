f260b5f60d9059fd5580d6dd57e17d8952cdb327
myakisheff <n.s.myakishev@yandex.ru>
2024-02-08 10:24:02 +0300

    Implemented new methods

--- src/main/kotlin/com/wavecollapse/persistence/repository/ListImageRepository.kt
+++ src/main/kotlin/com/wavecollapse/persistence/repository/ListImageRepository.kt
@@ -6,20 +6,40 @@ import java.util.*
 class ListImageRepository : ImageRepository {
     // fake implementation of local storage
     // For ideal: use persistence.model for saving to DB
-    private val images : MutableMap<UUID, Image> = mutableMapOf()
+    private val storage : MutableMap<UUID, Image> = mutableMapOf()
 
-    override fun save(image: Image) {
-        images[image.id] = image
+    override fun save(image: Image) : Image {
+        storage[image.id] = image
+        return image
     }
 
-    override fun getAll(): Map<UUID, Image> = images
-    override fun findById(id: UUID): Image? = images[id]
-
-    override fun getUniqueID(): UUID {
-        var id = UUID.randomUUID()
-        while(images.containsKey(id))
-            id = UUID.randomUUID()
-        return id
+    override fun getAll(): MutableIterable<Image> = storage.values
+    override fun findById(id: UUID): Image? = storage[id]
+    override fun deleteById(id: UUID) {
+        storage.remove(id)
     }
 
+    override fun deleteAll() {
+        storage.clear()
+    }
+
+    override fun saveAll(tiles: MutableIterable<Image>): MutableIterable<Image> {
+        tiles.forEach { storage[it.id] = it }
+        return tiles
+    }
+
+    override fun count(): Long = storage.size.toLong()
+
+    override fun findAllById(ids: MutableIterable<UUID>): MutableIterable<Image> =
+        storage
+            .filterKeys { it in ids }
+            .values
+            .toMutableList()
+
+    override fun existsById(id: UUID): Boolean =
+        storage.containsKey(id)
+
+    override fun delete(image: Image) {
+        storage.remove(image.id)
+    }
 }
--- src/main/kotlin/com/wavecollapse/persistence/repository/ListTileRepository.kt
+++ src/main/kotlin/com/wavecollapse/persistence/repository/ListTileRepository.kt
@@ -4,21 +4,42 @@ import main.kotlin.com.wavecollapse.business.Tile
 import java.util.UUID
 
 class ListTileRepository : TileRepository {
-    // fake implementation of local storage
+    // fake implementation
     // For ideal: use persistence.model for saving to DB
-    private val tiles : MutableMap<UUID, Tile> = mutableMapOf()
+    private val storage : MutableMap<UUID, Tile> = mutableMapOf()
 
-    override fun save(tile: Tile) {
-        tiles[tile.id] = tile
+    override fun save(tile: Tile) : Tile {
+        storage[tile.id] = tile
+        return tile
     }
 
-    override fun getAll(): Map<UUID, Tile> = tiles
+    override fun getAll(): MutableIterable<Tile> = storage.values
+    override fun deleteById(id: UUID) {
+        storage.remove(id)
+    }
 
-    override fun findById(id: UUID): Tile? = tiles[id]
-    override fun getUniqueID(): UUID {
-        var id = UUID.randomUUID()
-        while(tiles.containsKey(id))
-            id = UUID.randomUUID()
-        return id
+    override fun deleteAll() {
+        storage.clear()
+    }
+
+    override fun saveAll(tiles: MutableIterable<Tile>): MutableIterable<Tile> {
+        tiles.forEach { storage[it.id] = it }
+        return tiles
+    }
+
+    override fun count(): Long = storage.size.toLong()
+
+    override fun findById(id: UUID): Tile? = storage[id]
+    override fun findAllById(ids: MutableIterable<UUID>): MutableIterable<Tile> =
+        storage
+            .filterKeys { it in ids }
+            .values
+            .toMutableList()
+
+    override fun existsById(id: UUID): Boolean =
+        storage.containsKey(id)
+
+    override fun delete(tile: Tile) {
+        storage.remove(tile.id)
     }
 }
