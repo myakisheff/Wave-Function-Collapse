f2451c1ec6f3127ba6986ca840e889ffbfbd4c59
myakisheff <n.s.myakishev@yandex.ru>
2024-02-11 22:59:05 +0300

    added comment with a possible code

--- src/main/kotlin/com/wavecollapse/business/Tile.kt
+++ src/main/kotlin/com/wavecollapse/business/Tile.kt
@@ -5,4 +5,20 @@ import java.util.UUID
 class Tile (
     val id : UUID,
     val tile : Any,
-)
+){
+    // if every tile have connections
+    /*private val connections : MutableMap<Side, MutableList<UUID>> = mutableMapOf()
+
+    fun addConnection(side : Side, tileID : UUID)
+    {
+        connections[side]?.add(tileID) ?: {connections[side] = mutableListOf(tileID)}
+
+    }
+
+    fun addConnection(side : Side, tileIDs: List<UUID>)
+    {
+        tileIDs.forEach {
+            connections[side]?.add(it) ?: {connections[side] = mutableListOf(it)}
+        }
+    }*/
+}
